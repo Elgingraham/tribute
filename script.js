58aadfa1abd39839f4088bfa151184693653ab90
Elgingraham <elgin.graham@ocsbstudent.ca>
2021-02-19 15:06:21 +0000

    updated

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.getElementById('guess').addEventListener
 ('click',randomizer)
-let number = 0
-let userguess = 0
+let userinput = 0
+
 function randomizer() {
-userguess = document.getElementById('input').value
-userguess = parseInt(userguess)
+  userinput = document.getElementById('input')
+  .value
+  userinput = parseInt(userinput)
 
-
-number = (Math.random() * 6) + 1
-number = parseInt(number)
-
-if (userguess == number) {
-   document.getElementById('winner').innerHTML = "winner"
- }
+  if (userinput >= 17) {
+   document.getElementById('age').innerHTML = "You can see R rated movies alone."
+  } else if (userinput >= 13) {
+  document.getElementById('age').innerHTML = "You can see PG-13 movies alone."
+  } else if (userinput >= 5) {
+    document.getElementById('age').innerHTML = "You can see G or PG movies alone."
+  }else {
+    document.getElementById('age').innerHTML = "You are to young to understand most movies."
+  }
 }
