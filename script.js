9768a8be9dc47972c6f9bc928f55d5d31cb4bdb2
Elgingraham <elgin.graham@ocsbstudent.ca>
2021-02-19 15:57:30 +0000

    ddd

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ document.getElementById('guess').addEventListener('click', randomizer)
 let userinput = 0
 
 function randomizer () {
-     userinput = document.getElementById('input')
-     .value
-     userinput = parseInt(userinput)
+  userinput = document.getElementById('input')
+    .value
+  userinput = parseInt(userinput)
 
-   if (userinput >= 17) {
-     document.getElementById('age').innerHTML = 'You can see R rated movies alone.'
+  if (userinput >= 17) {
+    document.getElementById('age').innerHTML = 'You can see R rated movies alone.'
   } else if (userinput >= 13) {
-     document.getElementById('age').innerHTML = 'You can see PG-13 movies alone.'
+    document.getElementById('age').innerHTML = 'You can see PG-13 movies alone.'
   } else if (userinput >= 5) {
-     document.getElementById('age').innerHTML = 'You can see G or PG movies alone.'
+    document.getElementById('age').innerHTML = 'You can see G or PG movies alone.'
   } else {
-     document.getElementById('age').innerHTML = 'You are to young to understand most movies.'
+    document.getElementById('age').innerHTML = 'You are to young to understand most movies.'
   }
 }
